8c6ba565a76a44fa5b74361c3d6a0ffa1eb2564b
SHUBHAM LATHIYA <shubhamlathiya2004@gmail.com>
2024-10-08 23:10:44 +0530

    5 commit shubham

--- email_utils.py
+++ email_utils.py
@@ -1,16 +1,22 @@
-from flask_mail import Mail, Message
+from email.mime.text import MIMEText
+from flask_mail import Message, Mail
 from flask import current_app
 
-# Initialize the mail object
 mail = Mail()
 
-def send_email(subject, recipient_email, body):
+def send_email(subject, recipient_email, body_html):
     try:
-        msg = Message(subject, sender=current_app.config['MAIL_USERNAME'], recipients=[recipient_email])
-        msg.body = body
+        # Create the Flask-Mail message object
+        msg = Message(subject=subject,
+                      sender=current_app.config['MAIL_USERNAME'],
+                      recipients=[recipient_email])
 
-        # Send the email
+        # Attach the HTML body to the message
+        msg.html = body_html
+
+        # Send the email using Flask-Mail
         mail.send(msg)
+
         return True
     except Exception as e:
         print(f"Error sending email: {e}")
