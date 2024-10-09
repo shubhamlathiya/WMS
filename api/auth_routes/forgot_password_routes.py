c8d6ccb80812bece40a55a844d4baaf6e8dc6883
PriyankMangaroliya <priyankmangroliya1@gmail.com>
2024-10-09 22:56:20 +0530

    setting page to createing design

--- api/auth_routes/forgot_password_routes.py
+++ api/auth_routes/forgot_password_routes.py
@@ -26,11 +26,93 @@ def reset_password():
             token = s.dumps(email, salt='password-reset-salt')
             # print(token)  # Generate token
             base_url = "http://127.0.0.1:5000/forgotpassword/"
-            reset_link = f"{base_url}/reset-password/{token}"
+
+            body = f"""
+            <!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet"/>
+</head>
+<body style="margin: 0; font-family: 'Poppins', sans-serif; background: #ffffff; font-size: 14px;">
+<div style="max-width: 680px;margin: 0 auto;padding: 45px 30px 60px;background: #f4f7ff;
+        background-image: url(https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661497957196_595865/email-template-background-banner);
+        background-repeat: no-repeat;background-size: 800px 452px;background-position: top center;font-size: 14px;color: #434343;">
+    <header>
+        <table style="width: 100%;">
+            <tbody>
+            <tr style="height: 0;">
+                <td>
+                    <h1>WMS</h1>
+                </td>
+                <td style="text-align: right;">
+                    <span style="font-size: 16px; line-height: 30px; color: #ffffff;">Warehouse Management System</span>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </header>
+
+    <main>
+        <div style="margin: 0;margin-top: 70px;padding: 92px 30px 115px;background: #ffffff;border-radius: 30px;text-align: center;">
+            <div style="width: 100%; max-width: 489px; margin: 0 auto;">
+                <h1 style="margin: 0;font-size: 24px;font-weight: 500;color: #1f1f1f;">Password Forget Request</h1>
+                <p style="margin: 0;margin-top: 17px;font-size: 16px;font-weight: 500;">
+                    Hi {name},
+                </p>
+                <p style="margin: 0;margin-top: 17px;font-weight: 500;letter-spacing: 0.56px;">
+                    Forget Your Password?<br/>
+                    We received a request to reset the password for your account.<br/><br/>
+                    To reset your password, click on the link below:<br/>
+                    <span style="font-weight: 600; color: #1065f6;">{base_url}/reset-password/{token}</span><br/>
+                    If you did not forget your password you can safely ignore this email.
+                </p>
+            </div>
+        </div>
+
+        <p style="max-width: 400px; margin: 0 auto;margin-top: 90px;text-align: center;font-weight: 500;color: #8c8c8c;">
+            Need help? Ask at
+            <a href="mailto:wms@gmail.com" style="color: #499fb6; text-decoration: none;">wms@gmail.com</a>
+        </p>
+    </main>
+
+    <footer style="width: 100%;max-width: 490px;margin: 20px auto 0;text-align: center;border-top: 1px solid #e6ebf1;">
+        <p style="margin: 0;margin-top: 40px;font-size: 16px;font-weight: 600;color: #434343;">
+            Warehouse Management System
+        </p>
+        <p style="margin: 0; margin-top: 8px; color: #434343;"></p>
+        <div style="margin: 0; margin-top: 16px;">
+            <a href="" target="_blank" style="display: inline-block;">
+                <img width="36px" alt="Facebook"
+                     src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661502815169_682499/email-template-icon-facebook"/>
+            </a>
+            <a href="" target="_blank" style="display: inline-block; margin-left: 8px;">
+                <img width="36px" alt="Instagram"
+                     src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661504218208_684135/email-template-icon-instagram"/>
+            </a>
+            <a href="" target="_blank" style="display: inline-block; margin-left: 8px;">
+                <img width="36px" alt="Twitter"
+                     src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503043040_372004/email-template-icon-twitter"/>
+            </a>
+            <a href="" target="_blank" style="display: inline-block; margin-left: 8px;">
+                <img width="36px" alt="Youtube"
+                     src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503195931_210869/email-template-icon-youtube"/>
+            </a>
+        </div>
+        <p style="margin: 0; margin-top: 16px; color: #434343;">
+            Copyright © 2024 WMS. All rights reserved.
+        </p>
+    </footer>
+</div>
+</body>
+</html>
+            """
+
+            # reset_link = f"{base_url}/reset-password/{token}"
             # print(reset_link)
-            subject = "Password Reset Request"
+            subject = "Password Forget Request"
             # Send email with reset link
-            send_email(subject, email, reset_link)
+
+            send_email(subject, email, body)
 
             flash('A password reset link has been sent to your email.', 'success')
         else:
--- api/user_routes/user_routes.py
+++ api/user_routes/user_routes.py
@@ -28,6 +28,7 @@ def update_user_home(current_user, user_id):
 
     return render_template("user/update_user.html", user=user)
 
+
 @user.route('/viewuser', methods=['GET'], endpoint='viewuser')
 @token_required
 @role_required('users', 'view')
@@ -72,15 +73,86 @@ def add_personnel(current_user):
         mongo.db.users.insert_one(personnel)
 
         # Prepare email details
-        subject = 'Employee Details'
+        subject = 'User Details'
+
         body = f"""
-               Dear {name},
+               <!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet"/>
+</head>
+<body style="margin: 0; font-family: 'Poppins', sans-serif; background: #ffffff; font-size: 14px;">
+<div style="max-width: 680px;margin: 0 auto;padding: 45px 30px 60px;background: #f4f7ff;
+        background-image: url(https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661497957196_595865/email-template-background-banner);
+        background-repeat: no-repeat;background-size: 800px 452px;background-position: top center;font-size: 14px;color: #434343;">
+    <header>
+        <table style="width: 100%;">
+            <tbody>
+            <tr style="height: 0;">
+                <td>
+                    <h1>WMS</h1>
+                </td>
+                <td style="text-align: right;">
+                    <span style="font-size: 16px; line-height: 30px; color: #ffffff;">Warehouse Management System</span>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </header>
 
-               Your details have been successfully registered.
+    <main>
+        <div style="margin: 0;margin-top: 70px;padding: 92px 30px 115px;background: #ffffff;border-radius: 30px;text-align: center;">
+            <div style="width: 100%; max-width: 489px; margin: 0 auto;">
+                <h1 style="margin: 0;font-size: 24px;font-weight: 500;color: #1f1f1f;">User Added</h1>
+                <p style="margin: 0;margin-top: 17px;font-size: 16px;font-weight: 500;">
+                    Hello {name},
+                </p>
+                <p style="margin: 0;margin-top: 17px;font-weight: 500;letter-spacing: 0.56px;">
+                    Your details have been successfully registered.<br/>
+                    User Id : <span style="font-weight: 600; color: #1f1f1f;">{email}</span><br/>
+                    Mobile : <span style="font-weight: 600; color: #1f1f1f;">{mobile}</span><br/>
+                    Password : <span style="font-weight: 600; color: #1f1f1f;">{data.get('password')}</span><br/>
+                    Role : <span style="font-weight: 600; color: #1f1f1f;">{role}</span>.
+                </p>
+            </div>
+        </div>
 
-               Mobile: {mobile}
-               Role: {role}
-               Password: {data.get('password')} (Remember to change it after logging in)
+        <p style="max-width: 400px; margin: 0 auto;margin-top: 90px;text-align: center;font-weight: 500;color: #8c8c8c;">
+            Need help? Ask at
+            <a href="mailto:wms@gmail.com" style="color: #499fb6; text-decoration: none;">wms@gmail.com</a>
+        </p>
+    </main>
+
+    <footer style="width: 100%;max-width: 490px;margin: 20px auto 0;text-align: center;border-top: 1px solid #e6ebf1;">
+        <p style="margin: 0;margin-top: 40px;font-size: 16px;font-weight: 600;color: #434343;">
+            Warehouse Management System
+        </p>
+        <p style="margin: 0; margin-top: 8px; color: #434343;"></p>
+        <div style="margin: 0; margin-top: 16px;">
+            <a href="" target="_blank" style="display: inline-block;">
+                <img width="36px" alt="Facebook"
+                     src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661502815169_682499/email-template-icon-facebook"/>
+            </a>
+            <a href="" target="_blank" style="display: inline-block; margin-left: 8px;">
+                <img width="36px" alt="Instagram"
+                     src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661504218208_684135/email-template-icon-instagram"/>
+            </a>
+            <a href="" target="_blank" style="display: inline-block; margin-left: 8px;">
+                <img width="36px" alt="Twitter"
+                     src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503043040_372004/email-template-icon-twitter"/>
+            </a>
+            <a href="" target="_blank" style="display: inline-block; margin-left: 8px;">
+                <img width="36px" alt="Youtube"
+                     src="https://archisketch-resources.s3.ap-northeast-2.amazonaws.com/vrstyler/1661503195931_210869/email-template-icon-youtube"/>
+            </a>
+        </div>
+        <p style="margin: 0; margin-top: 16px; color: #434343;">
+            Copyright © 2024 WMS. All rights reserved.
+        </p>
+    </footer>
+</div>
+</body>
+</html>
                """
 
         # Send email
@@ -124,6 +196,7 @@ def update_user(current_user, user_id):
         # flash(f"Error updating user: {str(e)}", 'error')
         return redirect('/user/viewuser')
 
+
 @user.route('/user_status', methods=['POST'])
 def user_status():
     data = request.get_json()
@@ -143,6 +216,7 @@ def user_status():
         else:
             return jsonify({'success': False, 'message': 'Status update failed'})
     return jsonify({'success': False, 'message': 'Invalid request'}), 400
+
 
 @user.route('/viewclient', methods=['GET'], endpoint='viewclient')
 @token_required
