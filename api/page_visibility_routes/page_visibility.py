a316fe616a5ccc4954a98e971bfb4ac82320b290
PriyankMangaroliya <priyankmangroliya1@gmail.com>
2024-10-10 20:39:18 +0530

    Admin Module Changes

--- api/page_visibility_routes/page_visibility.py
+++ api/page_visibility_routes/page_visibility.py
@@ -1,6 +1,7 @@
 from bson import ObjectId
 from flask import Flask, request, jsonify, render_template, Blueprint
 from config import mongo
+from middleware.auth_middleware import token_required
 from middleware.page_visibility_middleware import role_required
 
 settings = Blueprint('settings', __name__)
@@ -8,15 +9,17 @@ settings = Blueprint('settings', __name__)
 
 @settings.route('/settings', methods=['GET'])
 # @role_required('stock_page')
-def settings_home():
+@token_required
+def settings_home(current_user):
     roles_data = mongo.db.users.distinct('role')
     print(roles_data)
     # return render_template("page_visibility/page_visibility.html")
     return render_template('page_visibility/page_visibility.html', roles=roles_data)
 
 
-@settings.route('/roles/<string:role>', methods=['GET'])
-def show_roles(role):
+@settings.route('/roles/<string:role>', methods=['GET'] ,endpoint='roles')
+@token_required
+def show_roles(current_user, role):
     # Fetch roles and page access data for the specified role
     roles_data = mongo.db.page_visibility.find()
 
@@ -31,8 +34,10 @@ def show_roles(role):
     print(role_permissions)
     return render_template('page_visibility/page.html', role=role, role_permissions=role_permissions)
 
-@settings.route('/update_permission', methods=['POST'])
-def update_permission():
+
+@settings.route('/update_permission', methods=['POST'],endpoint='update_permission')
+@token_required
+def update_permission(current_user):
     data = request.get_json()
     print(data)
     page_name = data['page_name']
